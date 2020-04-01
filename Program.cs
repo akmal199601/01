bb67abe4c247d7bbc677fa86366b03d3ab8d6d39
akmal199601 <akmal199601@mail.ru>
2020-04-02 00:22:34 +0500

    first

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace dz31._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("Введите длинну массива\n");
+            // int len = int.Parse(Console.ReadLine());
+            // int[] array = new int[len];
+            // for (int i = 0; i < array.Length; i++)
+            // {
+            //     array[i] = new Random().Next(0, 100);
+            //     Console.Write(array[i] + " ");
+            // }
+            // int min = array[0], max = array[0], sum = 0, mid = 0;
+            // System.Console.WriteLine("Не четкие ");
+            // foreach (var a in array)
+            // {
+            //     min = a < min ? a : min;
+            //     max = a > max ? a : max;
+            //     sum += a;
+            //     mid += a;
+            //     if (a % 2 != 0)
+            //     {
+            //         Console.Write(a + " ");
+            //     }
+            // }
+            // mid /= len;
+            // System.Console.WriteLine($"\nmin: {min}");
+            // System.Console.WriteLine($"max {max}");
+            // System.Console.WriteLine($"sum {sum}");
+            // System.Console.WriteLine($"mid {mid}");
+            // Console.ReadKey();
+
+
+            // //Задание 3
+
+            // int x = 0;
+            // for (int i = (len - 1); i >= (len % 2); i--)
+            // {
+            //     int a = array[i];
+            //     array[i] = array[x];
+            //     array[x] = a;
+            //     x++;
+            // }
+            // foreach (var item in array)
+            // {
+            //     Console.Write(item + " ");
+            // }
+            // Console.ReadKey();
+
+            // Задание  4
+            // System.Console.Write("Напишите длину массива:");
+            // int num = int.Parse(Console.ReadLine());
+            // int[] array = new int[num];
+            // for (int i = 0; i < num; i++)
+            // {
+            //     int Random = new Random().Next(1, 999);
+            //     array[i] = Random;
+            // }
+            // foreach (var item in array)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+            // System.Console.Write("index:");
+            // int index = int.Parse(Console.ReadLine());
+            // System.Console.Write("count:");
+            // int count = int.Parse(Console.ReadLine());
+            // int[] array2 = new int[count];
+            // int count2 = 0;
+
+            // for (int i = index; i < count + index; i++)
+            // {
+            //     if (i <= array.Length-1)
+            //     {
+            //         array2[count2] = array[i];
+            //         count2++;
+            //     }
+            //     else
+            //     {
+            //         array2[count2] = 1;
+            //     }
+            // }
+            // foreach (var item in array2)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
